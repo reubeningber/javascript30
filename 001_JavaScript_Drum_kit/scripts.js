aede4958b98d14e56eda1780c36e5d057c4f58ed
Reuben <reuben.ingber@gmail.com>
2017-04-21 16:25:37 -0400

    master

--- 001_JavaScript_Drum_kit/scripts.js
+++ 001_JavaScript_Drum_kit/scripts.js
@@ -1,5 +1,20 @@
-window.addEventListener('keydown', function(e){
-	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
-	console.log(audio);
-})
+function playSound(e) {
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	if (!audio) return; //Stop if no audio available
+	audio.currentTime = 0; //Rewind to start (play repeatedly)
+	audio.play();
+	key.classList.add('playing');
+}
+
+function removeTransition(e) {
+	if (e.propertyName !== 'transform') return; // Skip if its not a transform
+	this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound); 
