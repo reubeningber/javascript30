8703fae9633cc94cd7a68a6db4dcc4d47fdb5986
Reuben <reuben.ingber@gmail.com>
2017-04-21 17:00:15 -0400

    master Fix

--- 002_CSS_JS_Clock/scripts.js
+++ 002_CSS_JS_Clock/scripts.js
@@ -1,25 +1,27 @@
-const secondsHand = document.querySelector('.second-hand');
-const minsHand = document.querySelector('.min-hand');
-const hoursHand = document.querySelector('.hour-hand');
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
-function setDate(secondsHand, minsHands, hoursHand) {
+function setDate() {
 	const now = new Date();
 	
 	//Set Seconds
 	const seconds = now.getSeconds();
 	const secondsDegrees = ((seconds / 60) * 360) + 90; 
-	secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
 	//Set Minutes
 	const mins = now.getMinutes();
 	const minsDegrees = ((mins / 60) * 360) + 90;
-	minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+	minHand.style.transform = `rotate(${minsDegrees}deg)`;
 
 	//Set Hours
 	const hours = now.getHours();
 	const hoursDegrees = ((hours / 60) * 360) + 90;
-	hoursHand.style.transform = `rotate(${hoursDegrees}deg)`
+	hourHand.style.transform = `rotate(${hoursDegrees}deg)`
 };
 
-setInterval(setDate(secondsHand, minsHand, hoursHand), 1000);
+setInterval(setDate(), 1000);
+
+setDate();
 
